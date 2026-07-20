7b0c1803ea3a2b04c285ecc2105a4bccccdb0f7f
C Ming <aa89976566@gmail.com>
2026-07-20 13:00:06 +0000

    Deploy Crofton Books site

---
+++ _next/static/Zkg9q28g550KKAk18vELc/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/crofton-bookstore//_next/:path+",
+        "destination": "/crofton-bookstore/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
